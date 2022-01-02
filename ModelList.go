3c407183b5a05598a969e056d17e690acd12300f
Aiden Hadisi <so.hadisi@gmail.com>
2022-01-02 14:51:27 -0800

    update model list

--- ModelList.go
+++ ModelList.go
@@ -1,7 +1,29 @@
 package dataful
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type ModelList struct {
-	List []BaseModel
+	List []Model
 
 	r Repository
+}
+
+func NewModelList(r Repository, models []Model) *ModelList {
+	return &ModelList{
+		r:    r,
+		List: models,
+	}
+}
+
+func (ml *ModelList) R() Repository {
+	return ml.r
+}
+
+func (ml *ModelList) FindAll(ctx context.Context, filter bson.M, opts ...*options.FindOptions) error {
+	return ml.R().FindAll(ctx, filter, ml.List, opts...)
 }
