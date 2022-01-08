2bf045c246488f4ca718505109457ef85cf0d9fb
Aiden Hadisi <so.hadisi@gmail.com>
2022-01-08 13:32:44 -0800

    add in collection names

--- collections.go
+++ collections.go
@@ -6,4 +6,7 @@ const (
 	CollectionUser    Collection = "user"
 	CollectionChannel Collection = "channel"
 	CollectionCommand Collection = "command"
+	CollectionFilters Collection = "filters"
+	CollectionPurge   Collection = "purge"
+	CollectionAlerts  Collection = "alerts"
 )
